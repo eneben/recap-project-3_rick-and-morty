f1978d9d7a1ae2b344ad5f283cca8b2c0488dc4e
Elisabeth Neben <elisabeth.neben@posteo.de>
2024-05-27 10:42:47 +0200

    first version of charactercard function

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,30 +1,33 @@
-const main = document.querySelector('[data-js="main"]');
+const cardList = document.querySelector('[data-js="card-container"]');
 
-export function CharacterCard() {
-  const newCard = document.createElement("ul");
-  newCard.classList.add("card-container");
-  newCard.innerHTML = `<li class="card">
+export function CharacterCard(url, name, status, type, episode) {
+  const newCard = document.createElement("li");
+  newCard.classList.add("card");
+  newCard.innerHTML = `
   <div class="card__image-container">
     <img
       class="card__image"
-      src="${XXXXXXXXXXXXXX}"
+      src="${url}"
       alt="Rick Sanchez"
     />
     <div class="card__image-gradient"></div>
   </div>
   <div class="card__content">
-    <h2 class="card__title">${XXXXXXXXXXXXXX}</h2>
+    <h2 class="card__title">${name}</h2>
     <dl class="card__info">
       <dt class="card__info-title">Status</dt>
-      <dd class="card__info-description">${XXXXXXXXXXXXXX}</dd>
+      <dd class="card__info-description">${status}</dd>
       <dt class="card__info-title">Type</dt>
-      <dd class="card__info-description">${XXXXXXXXXXXXXX}</dd>
+      <dd class="card__info-description">${type}</dd>
       <dt class="card__info-title">Occurrences</dt>
-      <dd class="card__info-description">${XXXXXXXXXXXXXX}</dd>
+      <dd class="card__info-description">${episode.length}</dd>
     </dl>
   </div>
-</li>`;
-  main.append(newCard);
+`;
+  cardList.append(newCard);
 }
 
-CharacterCard();
+// const url =
+//   "https://upload.wikimedia.org/wikipedia/commons/d/d8/Person_icon_BLACK-01.svg";
+// const episode = ["keks", "krümel", "kuchen"];
+// CharacterCard(url, "John", "alive", "male", episode);
